0411df5621ff0c5f61e309e7da43db20e776bda1
Daniel Samson <daniel.samson@salesagility.com>
2017-03-28 10:33:15 +0100

    Update message-box.js

--- message-box.js
+++ message-box.js
@@ -40,6 +40,7 @@
      * @event ok
      */
     self.onOk = function () {
+      "use strict";
       $(self).trigger('ok');
       return false;
     };
@@ -48,6 +49,7 @@
      * @event cancel
      */
     self.onCancel = function () {
+      "use strict";
       $(self).trigger('cancel');
       return false;
     };
@@ -86,6 +88,7 @@
      * @return {jQuery}
      */
     self.getTitle = function() {
+      "use strict";
       return self.controls.modal.container.find('.modal-title');
     };
 
@@ -94,6 +97,7 @@
      * @param content htmlString|function
      */
     self.setBody = function(content) {
+      "use strict";
       self.controls.modal.container.find('.modal-body').html(content);
     };
 
@@ -101,6 +105,7 @@
      * @return {jQuery}
      */
     self.getBody = function() {
+      "use strict";
       return self.controls.modal.container.find('.modal-body');
     };
 
@@ -108,6 +113,7 @@
      *
      */
     self.showHeader = function() {
+      "use strict";
       return self.controls.modal.container.find('.modal-header').show();
     };
 
@@ -115,6 +121,7 @@
      *
      */
     self.hideHeader = function() {
+      "use strict";
       return self.controls.modal.container.find('.modal-header').hide();
     };
 
@@ -122,6 +129,7 @@
      *
      */
     self.showFooter = function() {
+      "use strict";
       return self.controls.modal.container.find('.modal-footer').show();
     };
 
@@ -129,6 +137,7 @@
      *
      */
     self.hideFooter = function() {
+      "use strict";
       return self.controls.modal.container.find('.modal-footer').hide();
     };
 
@@ -136,6 +145,7 @@
     self.footerContent = '<button class="button btn-ok" type="button">'+SUGAR.language.translate('','LBL_OK')+'</button> <button class="button btn-cancel" type="button">'+SUGAR.language.translate('','LBL_CANCEL_BUTTON_LABEL')+'</button> ';
 
     self.construct = function (constructOptions) {
+      "use strict";
       if(typeof self.controls.modal.container === "undefined") {
         if(typeof opts.onOK === "undefined") {
           opts.onOK = self.onOk;
@@ -215,6 +225,7 @@
      * @destructor
      */
     self.destruct = function () {
+      "use strict";
       self.attr('aria-hidden', "true");
       $('.modal-backdrop').last().remove();
       if($('.message-box').length <= 1) {
@@ -254,5 +265,6 @@
  * @param options
  */
 messageBox = function (options) {
+  "use strict";
   return $('<div></div>').appendTo('body').messageBox(options);
 }; 
